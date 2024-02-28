baf5549525e58bda873429af8f6f81f82787190b
ShaulChen <75518027+ShaulChen@users.noreply.github.com>
2024-02-28 14:31:28 +0800

    Add files via upload

---
+++ Method/Demo.java
@@ -0,0 +1,10 @@
+package Method;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
+
+    }
+}
